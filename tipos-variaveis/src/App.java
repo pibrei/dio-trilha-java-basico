ed241f8eb2e599fb99c0f87a50873ce4eeda34b7
Fabiano <thepibrei@gmail.com>
2024-06-21 23:00:28 -0300

    teste

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,12 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        byte idade = 123;
-        double idade2 = 123.5;
-        String nome = "Lucas";
-        double salario = 123.5;
-        System.out.println("My name is " + nome);
-        System.out.println("My age is " + idade);
-        System.out.println("My salary is " + salario);
+
     }
 }
