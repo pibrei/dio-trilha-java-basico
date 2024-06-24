5f89830369345fd857b6937886a8aabceb1f0802
Fabiano <thepibrei@gmail.com>
2024-06-24 08:56:32 -0300

    testes: aplicando conteúdo aprendido na aula

---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,8 @@
+public class Metodos {
+    public double somar (int num1, int num2 ) {
+        return num1 + num2;
+    }
+    public void imprimir (String texto) {
+        System.out.println(texto);
+    }
+}
